6117d65797f8d29c4d5b671a0de2a05db675fed9
nikkiregimbal <nicole.regimbal@mail.utoronto.ca>
2025-09-09 10:19:06 -0400

    Connected github PAT

--- ProdRepro.R
+++ ProdRepro.R
@@ -1,6 +1,12 @@
 library(renv)
 
+#Run this to create personal access token with github
+#install.packages("gitcreds")
+#library(gitcreds)
+#gitcreds_set()
+
 library(lterdatasampler) #this contains dataset options
 #write.csv(df, "and_vertebrates.csv") saving chosen dataset
 
 df <- read.csv("and_vertebrates.csv")
+
